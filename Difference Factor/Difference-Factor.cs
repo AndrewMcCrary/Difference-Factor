5a3ab2ce309ac8172c232b602f2d00c549f4af38
Andrew McCrary <ajmccrary03@gmail.com>
2020-01-27 14:27:42 -0500

    Initialized everything and began tested method

---
+++ Difference Factor/Difference-Factor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Difference_Factor
+{
+    public class Difference_Factor
+    {
+        // should return both sides of the string in order in an array
+        public static string[] FindNextLargestSubstrings(string str)
+        {
+
+        }
+    }
+}
