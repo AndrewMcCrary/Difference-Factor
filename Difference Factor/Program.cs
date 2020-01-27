521756293f59fbf90ac5801ca52eabca2455dfd8
Andrew McCrary <ajmccrary03@gmail.com>
2020-01-27 14:23:41 -0500

    Add project files.

---
+++ Difference Factor/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Difference_Factor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
