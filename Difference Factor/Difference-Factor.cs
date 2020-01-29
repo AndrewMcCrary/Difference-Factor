51f3e2419ef922d3d167a52ee89eda2eb8394ecd
bohnenfuhrer <48538198+bohnenfuhrer@users.noreply.github.com>
2020-01-28 21:56:58 -0500

    All finished

--- Difference Factor/Difference-Factor.cs
+++ Difference Factor/Difference-Factor.cs
@@ -1,47 +1,96 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace Difference_Factor
 {
     public class Difference_Factor
     {
-        const string ALPHA = "abcdefghijklmnopqrstuvwxyz";
-        const string ALPHA_UPPER = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        const string NUMERICS = "0123456789";
-        const char SPACE = ' ';
-
-        /// <summary>
-        /// Returns the starting index and length of the substrings
-        /// </summary>
-        /// <param name="str"></param>
-        /// <returns>First int type is the index of substring, second int type is the length</returns>
-        public static Tuple<int, int> FindNextLargestSubstrings(string str)
+        public static int ADFRecursively(string str1, string str2, ref int ADF, bool prepped = false)
         {
-            // Rotate through string left to right, searching for the same character
-            // Every substring must begin with the same character, and be the same length
-            // Need inner loop to start from right side to eliminate case "xxxxxxxxxxxxxxxxxxx"
-
-            if (str.Length <= 0)
-                throw new ArgumentOutOfRangeException();
-            
-
-
-            for (int i = 0; i < str.Length; i++)
+            // prep strings, replaces numbers and spaces with string.empty, and converts to upper
+            if (!prepped)
             {
-                // First loop rotates through string establishing base character
-
-                for (int j = str.Length - 1; j > i; j--) 
-                {
-                    // Second loop gives character to compare to first
-                        Console.WriteLine(str[i] + " : " + str[j]);
-                    
-                }
+                str1 = new string(str1.Where(x => char.IsLetter(x)).ToArray()).ToUpper();
+                str2 = new string(str2.Where(x => char.IsLetter(x)).ToArray()).ToUpper();
             }
 
-            return new Tuple<int, int>(0, 0);
+            string longest = FindLongestSubstring(str1, str2); // if null, there are no more substrings present
+            if (longest is null || longest == string.Empty)
+                return ADF;
+            
+            
+            ADF += longest.Length;
+
+
+            // Splits strings
+
+            // First string
+            string a1 = str1.Substring(0, str1.IndexOf(longest));
+            string a2 = string.Empty;
+            if (a1.Length + longest.Length < str1.Length) //checks for empty back half
+                a2 = str1.Substring(str1.IndexOf(longest) + longest.Length, str1.Length - a1.Length - longest.Length);
+
+            // Second string
+            string b1 = str2.Substring(0, str2.IndexOf(longest));
+            string b2 = string.Empty;
+            if (b1.Length + longest.Length < str2.Length) //checks for empty back half
+                b2 = str2.Substring(str2.IndexOf(longest) + longest.Length, str2.Length - b1.Length - longest.Length);
+
+            ADFRecursively(a2, b2, ref ADF, true); // Back half of both strings
+            ADFRecursively(a1, b1, ref ADF, true); // Front half of both strings
+
+            return ADF;
+
+        }
+
+
+        public static string FindLongestSubstring(string str1, string str2) // Absolutely terrible but it works
+        {
+            if (str1 == str2)
+                return str1;
+            if (str1 == string.Empty || str2 == string.Empty || str1 is null || str2 is null)
+                return null;
+
+            List<string> substrings = new List<string>();
+
+            for (int i = 0; i < str1.Length; i++)
+            {
+                for (int j = 0; j < str2.Length; j++)
+                {
+                    if (str1[i] == str2[j])
+                    {
+                        int initI = i;
+                        int initJ = j;
+                        string ss = string.Empty;
+                        do
+                        {
+                            ss += str1[i].ToString();
+                            i++;
+                            j++;
+                            if (i >= str1.Length || j >= str2.Length)
+                                break;
+                            
+                        } while (str1[i] == str2[j]);
+                        i = initI;
+                        j = initJ;
+                        substrings.Add(ss); // GC will get ss, initI, initJ
+                    }
+                }
+            }
+            if (substrings.Count == 0)
+                return null;
+
+            //foreach (var i in substrings)
+            //    Console.WriteLine(i);
+
+            //substrings = substrings.OrderBy(x => x.Length).Reverse().Where(x => x.Length == substrings[0].Length).OrderBy(x => x).ToList();
+            substrings = substrings.OrderBy(x => x.Length).Reverse().ToList();
+            substrings.RemoveAll(x => x.Length < substrings[0].Length);
+            substrings.Sort();
+
+            return substrings.First();
         }
     }
 }
--- Difference Factor/Program.cs
+++ Difference Factor/Program.cs
@@ -1,17 +1,86 @@
 ﻿using System;
+using System.Text.RegularExpressions;
+using System.IO;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Difference_Factor
 {
     class Program
     {
+        // need to implement input through a file ".txt" and only running once
         static void Main(string[] args)
         {
-            Difference_Factor.FindNextLargestSubstrings("abcdef");
-            Console.ReadKey();
+            if (!File.Exists("./input.txt"))
+                File.CreateText("./input.txt").Close();
+            Help();
+            string buffer = string.Empty;
+            while (true)
+            {
+                string raw_input = Console.ReadLine();
+                switch (raw_input.ToLower())
+                {
+                    case "help":
+                        Help();
+                        break;
+                    case "clear":
+                        Console.Clear();
+                        break;
+                    case "":
+                    default:
+                        if (!File.Exists("./input.txt"))
+                        {
+                            Console.WriteLine("File not found, making a new one.");
+                            File.CreateText("./input.txt").Close();
+                        }
+                        try
+                        {
+                            buffer = File.ReadAllText("./input.txt");
+                        } catch (Exception e) { Console.WriteLine("Oops, you made a exception:\n\n" + e.Message); }
+
+                        ParseandExecute(buffer);
+                        buffer = string.Empty;
+                        break;
+                }
+            }
+            
+
+        }
+
+        public static void Help()
+        {
+            Console.WriteLine("FORMAT DIRECTIONS:\nSeparate different strings on the same input via the enter key.\nPress # on a new line to separate input streams.");
+            Console.WriteLine("Example:\n\nString1\nString2\n#\nSecondstring1\nSecondstring2\n\n");
+            Console.WriteLine("Type CLEAR to clear the console\nType HELP to show this screen again");
+            Console.WriteLine("Please fill the \"input.txt\" with input formatted in this way then press enter in the console to execute.\nYou may reuse the same text file for input, just remember to save.");
+        }
+
+        public static void ParseandExecute(string contents)
+        {
+            List<Tuple<string, string>> Input = new List<Tuple<string, string>>();
+
+            contents = contents.Replace("\r", "");
+            foreach (string i in contents.Split('#'))
+            {
+                if (i[0].ToString() == "\n")
+                    i.Remove(0, 1);
+                try
+                {
+                    if (i[0].ToString() == "\n")
+                        Input.Add(new Tuple<string, string>(i.Remove(0, 1).Split("\n".ToCharArray()[0])[0], i.Remove(0, 1).Split("\n".ToCharArray()[0])[1]));
+                    else
+                        Input.Add(new Tuple<string, string>(i.Split("\n".ToCharArray()[0])[0], i.Split("\n".ToCharArray()[0])[1]));
+
+                } catch (IndexOutOfRangeException) { Console.WriteLine("Looks like you have a format error"); return; }
+                catch (Exception) { Console.WriteLine("Looks like you have an unknown error!"); }
+            }
+
+            int buff = 0;
+            foreach (var i in Input)
+            {
+                Console.WriteLine(Difference_Factor.ADFRecursively(i.Item1, i.Item2, ref buff));
+                buff = 0;
+            }
+            Console.WriteLine("\nExecuted successfully!");
         }
     }
 }
